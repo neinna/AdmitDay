1a5b76ca6f4f8b66179060144fa72237821aa10f
neinna <neinna@gmail.com>
2026-04-08 17:55:19 +0000

    Add Sentry error monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from '@/components/NavBar'
@@ -5,9 +6,14 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'ListReady',
-  description: "Find NYC public high schools that match your student's profile.",
+export function generateMetadata(): Metadata {
+  return {
+    title: 'ListReady',
+    description: "Find NYC public high schools that match your student's profile.",
+    other: {
+      ...Sentry.getTraceData()
+    }
+  }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
